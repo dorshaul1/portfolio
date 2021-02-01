eb295a246ce29aa3c8c3ae5bc0486eb4aebf0a51
dorshaul1 <dor.asti@gmail.com>
2021-02-01 15:09:17 +0200

    adding data model to prodfolios

--- js/ca-gallery.js
+++ js/ca-gallery.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -27,12 +27,38 @@
   });
 
   // Collapse the navbar when page is scrolled
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
     }
+    renderProtfolio()
   });
 
 })(jQuery); // End of use strict
+
+function renderProtfolio() {
+  var protfolios = getProtfolios()
+  var elPortfoliosList = $('.portfolios-list')
+  
+  protfolios.map(function (protfolio){
+    strHTML = `<div class="col-md-4 col-sm-6 portfolio-item">
+    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+    <div class="portfolio-hover">
+    <div class="portfolio-hover-content">
+    <i class="fa fa-plus fa-3x"></i>
+    </div>
+    </div>
+    <img class="img-fluid" src=${protfolio.imgUrl} alt="">
+    </a>
+    <div class="portfolio-caption">
+    <h4>${protfolio.name}</h4>
+    <p class="text-muted">${protfolio.title}</p>
+    </div>
+    </div>`
+  })
+  // console.log('strHTML:', strHTML)
+
+  elPortfoliosList.html(strHTML)
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log('Starting up');
+'use strict'
+
+var gProtfolios = []
+
+var sokoban = createProtfolio("Threads", "Threads", "Illustration", "lorem ipsum lorem ipsum lorem ipsum", "projs/sokoban", 1448693940000,"img/portfolio/01-thumbnail.jpg" ,["Matrixes", "keyboard events"])
+gProtfolios.push(sokoban)
+
+// console.log('gProtfolios:', gProtfolios)
+function createProtfolio(id, name, title, desc, url, publishdAt,imgUrl = null, labels) {
+
+    var protfolio = {
+        id: id,
+        name: name,
+        title: title,
+        desc: desc,
+        url: url,
+        publishedAt: publishdAt,
+        imgUrl: imgUrl,
+        labels: labels,
+    }
+
+    return protfolio
+}
+
+function getProtfolios(){
+    return gProtfolios
+}
